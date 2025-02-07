53baae25ed5f3f3b899613beb7a14d3131919467
artosousa <artosousa@gmail.com>
2025-02-06 22:06:00 -0500

    updated api server stuff

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,23 +1,25 @@
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 
-const { privateKey } = JSON.parse(import.meta.env.FIREBASE_PRIVATE_KEY);
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : undefined;
 const activeApps = getApps();
 const serviceAccount = {
   type: "service_account",
-  project_id: import.meta.env.FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   privateKey,
-  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
-  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
 };
 
 const initApp = () => {
-  if (import.meta.env.PROD) {
+  if (process.env.PROD) {
     console.info("PROD env detected. Using default service account");
     return initializeApp();
   }
