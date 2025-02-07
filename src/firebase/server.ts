a57910c83725dedca8b8dc59f84f45a9fb95b57c
artosousa <artosousa@gmail.com>
2025-02-06 21:36:35 -0500

    added prod check

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,18 +1,13 @@
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 
-// Ensure Firebase is only initialized once
+const { privateKey } = JSON.parse(import.meta.env.FIREBASE_PRIVATE_KEY);
 const activeApps = getApps();
-
-// Properly format the private key (fixes JSON parsing issues)
-const privateKey = import.meta.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-
-// Define the service account object
 const serviceAccount = {
   type: "service_account",
   project_id: import.meta.env.FIREBASE_PROJECT_ID,
   private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: privateKey,
+  privateKey,
   client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
   client_id: import.meta.env.FIREBASE_CLIENT_ID,
   auth_uri: import.meta.env.FIREBASE_AUTH_URI,
@@ -21,23 +16,15 @@ const serviceAccount = {
   client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
 };
 
-// Function to initialize Firebase Admin SDK
 const initApp = () => {
-  if (activeApps.length > 0) {
-    console.info("Firebase Admin is already initialized.");
-    return activeApps[0];
-  }
-
   if (import.meta.env.PROD) {
-    console.info("PROD environment detected. Using default service account.");
+    console.info("PROD env detected. Using default service account");
     return initializeApp();
   }
-
-  console.info("Initializing Firebase Admin SDK with service account.");
+  console.info("Loading service account from env.");
   return initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
   });
 };
 
-// Initialize Firebase app
-export const app = initApp();
+export const app = activeApps.length === 0 ? initApp() : activeApps[0];
